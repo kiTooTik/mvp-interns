9945dbf7440313b8b0104632ed0434da7a785e89
kiTooTik <markiTooTik@gmail.com>
2026-03-03 22:44:54 +0800

    chore: final cleanup of diagnostic logs

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Diagnostic: log env vars to console to verify they were embedded at build time
-console.log("[ENV CHECK] VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL ?? "(undefined)");
-console.log("[ENV CHECK] VITE_SUPABASE_PUBLISHABLE_KEY:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ? "(present)" : "(undefined)");
-
 createRoot(document.getElementById("root")!).render(<App />);
+
 
